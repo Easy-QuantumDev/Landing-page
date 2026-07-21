583a3c5ca987bc694dbb883e6f350ecfa6a10b69
mrrelax <mohammadrezabarati1084@gmail.com>
2026-07-22 03:07:29 +0430

    just start the js and add  hamburger menu

---
+++ Ai-Landing-Page/script.js
@@ -0,0 +1,7 @@
+const hamburger_btn = document.querySelector(".hamburger");
+const right_nav = document.querySelector(".right-nav");
+
+hamburger_btn.addEventListener("click", () => {
+    right_nav.classList.toggle("active");
+    console.log(right_nav.classList);
+});
